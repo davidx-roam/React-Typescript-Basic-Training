2aac09f66e5845394c04397d5c1b130a1930956a
davidx-roam <david.xu@roamdigital.com>
2023-01-10 14:37:58 +1300

    updating the onSubmit question

--- src/components/14-On-Submit.tsx
+++ src/components/14-On-Submit.tsx
@@ -1,36 +1,30 @@
-//TODO: Add typescript to this file
+//TODO: 1 - Add typescript to this file
+//TODO: 2- Update the code so that clicking on 'Click Me' Increment the count
+//TODO: 3- Update the code so the current count is logged out in the console when 'Submit' is clicked
 
 import React from "react";
 
-interface ClickableProps {
-  handleClick: React.MouseEventHandler<HTMLButtonElement>;
-}
-
-const Clickable = ({ handleClick }: ClickableProps) => {
-  return (
-    <button type="button" onClick={handleClick}>
-      Click Me!
-    </button>
-  );
+const Clickable = ({ handleClick }) => {
+  return <button onClick={handleClick}>Click Me!</button>;
 };
 
 export const Exercise = () => {
   const [count, setCount] = React.useState<number>(0);
-  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+  const handleClick = (e) => {
     setCount(count + 1);
   };
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    console.log(count);
+  const handleSubmit = (e) => {
+    //TODO: Implement
   };
 
   return (
     <>
       <h2>Exercise 14</h2>
+      {count}
       <form onSubmit={handleSubmit}>
         <Clickable handleClick={handleClick} />
-        <button type="submit">Submit</button>
+        <button>Submit</button>
       </form>
     </>
   );
