155465ee97adf373946127fe69dd92cd4bcb68f1
davidx-roam <david.xu@roamdigital.com>
2023-01-10 16:26:31 +1300

    create useRef question

--- src/components/15-Hooks-useRef.tsx
+++ src/components/15-Hooks-useRef.tsx
@@ -1,15 +1,10 @@
 // TODO: Modify the code so that on page load, the second input field receives focus
 // TODO: Add typescript to this file
 
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 export const Exercise = () => {
-  const ref = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.focus();
-    }
-  }, []);
+  const ref = useRef(null);
   return (
     <>
       <h2>Exercise 15</h2>
