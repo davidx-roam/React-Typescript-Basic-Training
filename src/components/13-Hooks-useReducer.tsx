c81b846edc52b8258aa184fccb0e60f2b1cdacd7
davidx-roam <david.xu@roamdigital.com>
2023-01-10 14:24:36 +1300

    useReducer problem

--- src/components/13-Hooks-useReducer.tsx
+++ src/components/13-Hooks-useReducer.tsx
@@ -2,29 +2,12 @@
 
 import React from "react";
 
-interface ClickableProps {
-  label: string;
-  handleClick: () => void;
-}
-
-const Clickable = ({ handleClick, label }: ClickableProps) => {
+const Clickable = ({ handleClick, label }) => {
   return <button onClick={handleClick}>{label}</button>;
 };
 
-type ActionType = "increment" | "decrement";
-interface StateType {
-  count: number;
-}
-
-const reducer = (state: StateType, action: ActionType) => {
-  switch (action) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    default:
-      throw new Error();
-  }
+const reducer = (state, action) => {
+  //TODO: create a reducer that accept two types of actions - 'increment' and 'decrement'
 };
 
 export const Exercise = () => {
