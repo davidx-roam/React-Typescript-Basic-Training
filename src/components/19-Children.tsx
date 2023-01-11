0d863fe69afc21b76e6bb9f26ade2b5b5166f840
davidx-roam <david.xu@roamdigital.com>
2023-01-11 17:46:04 +1300

    Adding Children Question

--- src/components/19-Children.tsx
+++ src/components/19-Children.tsx
@@ -1,35 +1,35 @@
-//TODO: Fix all error in this file
-interface RocketProps {
-  name: string;
-  children: React.ReactNode;
-}
-
-const Rocket = ({ name, children }: RocketProps) => {
-  return (
-    <div>
-      <h3>{name}</h3>
-      {children}
-    </div>
-  );
+//TODO: Add Typescript and fix all error in this file
+const Rocket = ({ name, children }) => {
+  <div>
+    <h3>{name}</h3>
+    {children}
+  </div>;
 };
 
 //TODO: Serial accepts a string that starts with `BOOST-` followed by numbers
-const Booster = ({ serial }: { serial: `BOOST-${number}` }) => (
-  <h4>Booster: {serial}</h4>
+const Booster = ({ serial }) => (
+  <>
+    <h4>Booster: {serial}</h4>
+    <sub>{serial}</sub>
+  </>
 );
 
-const Shuttle = () => {
-  return <h4>Shuttle</h4>;
+//TODO: Serial accepts a string that starts with `BOOST-` followed by numbers
+const Shuttle = ({ serial }) => {
+  <>
+    <h4>Shuttle</h4>
+    <sub>{serial}</sub>
+  </>;
 };
 
 export const Exercise = () => (
   <>
     <h2>Exercise 19</h2>
     <Rocket name="Roam Force 1">
-      <Shuttle></Shuttle>
+      <Shuttle serial="BOOST-7365"></Shuttle>
       <Booster serial="BOOST-1345" />
       <Booster serial="BOOST-1234" />
-      <Booster serial="BOOST-2147" />
+      <Booster serial="BOOST-2I47" />
     </Rocket>
   </>
 );
