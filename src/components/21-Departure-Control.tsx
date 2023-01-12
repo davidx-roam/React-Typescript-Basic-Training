a03c63cb576c3bd8685e4221f738bb55c437c611
davidx-roam <david.xu@roamdigital.com>
2023-01-12 15:26:13 +1300

    Departure Control Question

--- src/components/21-Departure-Control.tsx
+++ src/components/21-Departure-Control.tsx
@@ -1,18 +1,12 @@
 //TODO: Create a landing and departure control system to manage the traffic coming and going from a space port
-import { useState, useEffect } from "react";
-
-type Status = "requestLanding" | "landed" | "onLaunchPad";
-type SpacePortRecord = Record<string, Status>;
-interface SpacePortProps {
-  portName: string;
-}
+import { useState } from "react";
 
 // TODO: Add typescript to this function
 // This is a mock API for fetching what ships are in the spaceport, and their status
 // The ships can have any string as their name (key),
 // they can only one of three statuses `requestLanding`, `landed`, `onLaunchPad`.
 const fetchPortRecord = () => {
-  const portRecord: SpacePortRecord = {
+  const portRecord = {
     "Roam Force One": "requestLanding",
     "The TARDIS": "landed",
     "USS Enterprise (NC-1701)": "onLaunchPad",
@@ -21,26 +15,12 @@ const fetchPortRecord = () => {
   return portRecord;
 };
 
-interface ShipStatusProps {
-  name: string;
-  status: Status;
-  updateStatus: (name: string) => void;
-}
-
 //TODO: Add typescript to this component and complete its functionality
 // ShipStatus displays the status for one ship (as the name implies)
 // As well a button to control its status. The button display 'approve', or 'prepare for departure' or 'launch'
 // depending on the ship's current status.
-const ShipStatus = ({ name, status, updateStatus }: ShipStatusProps) => {
-  const buttonText =
-    status === "requestLanding"
-      ? "approve"
-      : status === "landed"
-      ? "prepare for departure"
-      : status === "onLaunchPad"
-      ? "launch"
-      : "";
-
+const ShipStatus = ({ name, status, updateStatus }) => {
+  const buttonText = "<PLACEHOLDER>";
   return (
     <li>
       <label>{`${name} --- ${status}`}</label>
@@ -55,38 +35,13 @@ const ShipStatus = ({ name, status, updateStatus }: ShipStatusProps) => {
 // This custom hook keeps track of all the ships in the spaceport and update their status
 // it accepts an initial state as input, and it exposes
 // record - the status for all ships in the port
-// updateEntry - update the status for one ship
+// updateEntry - update the status for one ship to the next logical status
 // addNewShip - add a new ship entry to the record
-const useSpacePortRecord = (initialState: SpacePortRecord | null) => {
-  const [record, setRecord] = useState<SpacePortRecord | null>(initialState);
-
-  useEffect(() => {
-    setRecord(fetchPortRecord());
-  }, []);
-
-  const updateEntry = (name: string) => {
-    if (record) {
-      switch (record[name]) {
-        case "requestLanding":
-          setRecord({ ...record, [name]: "landed" });
-          break;
-        case "landed":
-          setRecord({ ...record, [name]: "onLaunchPad" });
-          break;
-        default:
-          const copy = { ...record };
-          delete copy[name];
-          setRecord(copy);
-      }
-    }
-  };
-
+const useSpacePortRecord = (initialState) => {
+  const [record, setRecord] = useState(initialState);
+  const updateEntry = (name: string) => {};
   const addNewShip = () => {
     const newShipName = `Visitor-${Math.trunc(Math.random() * 10000)}`;
-    setRecord({
-      ...record,
-      [newShipName]: "requestLanding",
-    });
   };
 
   return { record, updateEntry, addNewShip };
@@ -95,33 +50,16 @@ const useSpacePortRecord = (initialState: SpacePortRecord | null) => {
 // TODO: add typescript to this component
 // show 'Loading...' initially.
 // show 'No ship found' if all the ship has departed
-const SpacePort = ({ portName }: SpacePortProps) => {
+const SpacePort = ({ portName }) => {
   // Do not modify how this hook is being used, or change the initial value
   const { record, updateEntry, addNewShip } = useSpacePortRecord(null);
-
-  if (!record) {
-    return (
-      <div>
-        <h3>{portName} Spaceport Control</h3>
-        <p>No record found</p>
-      </div>
-    );
-  }
 
   const keys = Object.keys(record) as string[];
 
   return (
     <div>
       <h3>{portName} Spaceport Control</h3>
-      <ul>
-        {/* TODO: display the list of ship statuses here */}
-        {keys.map((key) => {
-          const status = record[key];
-          return (
-            <ShipStatus name={key} status={status} updateStatus={updateEntry} />
-          );
-        })}
-      </ul>
+      <ul>{/* TODO: display the list of ship statuses here */}</ul>
       {/* TODO: add new ship when button is clicked */}
       <button onClick={addNewShip}>New Spaceship Approaches</button>
     </div>
